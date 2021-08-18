4199dc048579e3d6847a5b58ce26ea985b1fa1a3
Stefouch <47865544+Stefouch@users.noreply.github.com>
2021-08-18 16:49:46 +0200

    log: cleaning

--- src/errors.js
+++ src/errors.js
@@ -7,21 +7,21 @@ import YearZeroRollManager from './YearZeroRollManager.js';
 export class GameTypeError extends TypeError {
   constructor(msg) {
     super(`Unknown game: "${msg}". Allowed games are: ${YearZeroRollManager.GAMES.join(', ')}.`);
-    this.name = 'YZ GameType Error';
+    this.name = 'YZUR | GameType Error';
   }
 }
 
 export class DieTypeError extends TypeError {
   constructor(msg) {
     super(`Unknown die type: "${msg}". Allowed types are: ${Object.keys(CONFIG.YZUR.DICE.DIE_TYPES).join(', ')}.`);
-    this.name = 'YZ DieType Error';
+    this.name = 'YZUR | DieType Error';
   }
 }
 
 // class RollError extends SyntaxError {
 //   constructor(msg, obj) {
 //     super(msg);
-//     this.name = 'YZ Roll Error';
+//     this.name = 'YZUR | Roll Error';
 //     if (obj) console.error(obj);
 //   }
 // }
