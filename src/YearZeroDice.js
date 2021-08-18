bc20f40ac1873659f2f8ba2ff6d8b34f50339927
Stefouch <47865544+Stefouch@users.noreply.github.com>
2021-08-18 16:50:15 +0200

    refactor(isYearZeroDie): return always true

--- src/YearZeroDice.js
+++ src/YearZeroDice.js
@@ -61,7 +61,8 @@ export class YearZeroDie extends Die {
    * @readonly
    */
   get isYearZeroDie() {
-    return this instanceof YearZeroDie;
+    // return this instanceof YearZeroDie;
+    return true;
   }
 
   /**
