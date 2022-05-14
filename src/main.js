afda58663637e3e2ccbc97be464b86356f63aee7
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-05-14 15:28:48 +0200

    refactor: remove YzurErrors from main export

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import YearZeroRollManager from './YearZeroRollManager.js';
 import YearZeroRoll from './YearZeroRoll.js';
 import * as YearZeroDice from './YearZeroDice.js';
 import YZUR from './constants.js';
-import * as YzurErrors from './errors.js';
+// import * as YzurErrors from './errors.js';
 
 // DO NOT ADD "default"
 // Here is why: https://stackoverflow.com/questions/43814830/destructuring-a-default-export-object
@@ -11,5 +11,5 @@ export {
   YearZeroRoll,
   YearZeroDice,
   YZUR,
-  YzurErrors,
+  // YzurErrors,
 };
