6a5d1f23c6b460cceb578ea35dc1b8ab40bd507c
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-05-14 14:31:54 +0200

    feat: licence banner with rollup

--- src/main.js
+++ src/main.js
@@ -1,32 +1,3 @@
-/*
- * ===============================================================================
- *  YZUR
- *    YEAR ZERO UNIVERSAL DICE ROLLER FOR THE FOUNDRY VTT
- * ===============================================================================
- * Author: @Stefouch
- * Version: 3.0.0     for:     Foundry VTT V8 (0.8.8)
- * Licence: MIT
- * ===============================================================================
- * Content:
- * 
- * - YearZeroRollManager: Interface for registering dice.
- * 
- * - YearZeroRoll: Custom implementation of the default Foundry Roll class,
- *     with many extra getters and utility functions.
- * 
- * - YearZeroDie: Custom implementation of the default Foundry DieTerm class,
- *     also with many extra getters.
- * 
- * - (Base/Skill/Gear/etc..)Die: Extends of the YearZeroDie class with specific
- *     DENOMINATION and LOCKED_VALUE constants.
- * 
- * - CONFIG.YZUR.game: The name of the game stored in the Foundry config.
- * 
- * - CONFIG.YZUR.DICE.ICONS.{..}: The dice labels stored in the Foundry config.
- * 
- * ===============================================================================
- */
-
 import YearZeroRollManager from './YearZeroRollManager.js';
 import YearZeroRoll from './YearZeroRoll.js';
 import * as YearZeroDice from './YearZeroDice.js';
