3826229b7e19762b8bada409ab5ac6127073b898
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-05-22 15:49:33 +0200

    feat: rollup removes inline typedef

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-// https://www.npmjs.com/package/rollup-plugin-license
+const eslint = require('@rollup/plugin-eslint');
 const license = require('rollup-plugin-license');
+const { readFileSync, writeFileSync } = require('fs');
 const { compatibleCoreVersion } = require('./system.json');
 
 module.exports = {
@@ -7,7 +8,27 @@ module.exports = {
   output: {
     file: './dist/yzur.js',
     format: 'es',
+    // https://github.com/rollup/plugins
     plugins: [
+      eslint({
+        throwOnError: true,
+      }),
+      // Inline plugin
+      {
+        name: 'rollup-plugin-remove-inline-typedef',
+        writeBundle(options) {
+          const date = new Date().toISOString().split('T')[1].split('.')[0];
+          console.log(`[${date}] Starting 'rollup-plugin-remove-inline-typedef'...`);
+          const data = readFileSync(options.file, { encoding: 'utf-8' })
+            .replace(/$\n^\/\*\* @typedef.*$/gm, match => {
+              console.log(`Remove line: ${match.replace('\n', '')}`);
+              return '';
+            })
+            .replace(/\n\n\n/gm, '\n\n');
+          writeFileSync(options.file, data, { encoding: 'utf-8' });
+        },
+      },
+      // https://www.npmjs.com/package/rollup-plugin-license
       license({
         // sourcemap: true,
         // cwd: '.',
